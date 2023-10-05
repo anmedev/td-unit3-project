5986fd06e8e1cbd5eb62b083a359ee4b59bca697
anmedev <aam2ds@virginia.edu>
2023-10-05 03:12:04 -0400

    add space before open curly brace on line 114

--- js/script.js
+++ js/script.js
@@ -111,7 +111,7 @@ const isValidName = () => nameField.value !== "";
 form.addEventListener("submit", (e) => {
   // Function to validate text field inputs.
   const validator = (elementInput, validation) => {
-    if (validation()){
+    if (validation()) {
         elementInput.closest('label').className = 'valid';
         elementInput.nextElementSibling.style.display= "none";
       } else {
